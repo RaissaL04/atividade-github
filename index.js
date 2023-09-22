653355b5a721a2fe1bb070bfae7480694fd6f990
CamilyKisner <145024387+RaissaL04@users.noreply.github.com>
2023-09-22 09:33:30 -0300

    codigo novo

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ if (!isNaN(numero)) {
 } else {
     console.log("Por favor, digite um número válido.");
 }
+// Solicitar um número ao usuário
+ numero = parseInt(prompt("Digite um número para calcular a tabuada e o fatorial:"));
+
+// Verificar se o número é válido
+if (!isNaN(numero)) {
+    // Imprimir a tabuada do número de 1 a 10
+    console.log(`Tabuada do ${numero}:`);
+    for (let i = 1; i <= 10; i++) {
+        const resultadoTabuada = numero * i;
+        console.log(`${numero} x ${i} = ${resultadoTabuada}`);
+    }
+
+    // Calcular o fatorial do número
+    let fatorial = 1;
+    for (let i = 1; i <= numero; i++) {
+        fatorial *= i;
+    }
+    console.log(`Fatorial de ${numero} = ${fatorial}`);
+} else {
+    console.log("Por favor, digite um número válido.");
+}
